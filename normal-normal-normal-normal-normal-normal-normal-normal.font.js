6e373e8956df6577b4a0126e465416622e27b340
Mariana Nunes <83194754+mariananvnes@users.noreply.github.com>
2022-11-30 02:07:16 +0000

    Add files via upload

---
+++ normal-normal-normal-normal-normal-normal-normal-normal.font.js
@@ -0,0 +1,232 @@
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright 2019 Dinamo (Johannes Breyer, Fabian Harb) & Erkin Karamemet. All
+ * rights reserved.
+ * 
+ * Trademark:
+ * Whyte is a trademark of Dinamo (Johannes Breyer, Fabian Harb).
+ * 
+ * Full name:
+ * WhyteInktrap-BoldItalic
+ * 
+ * Description:
+ * Please do not overuse Dinamo typefaces - keep them alive
+ * 
+ * Manufacturer:
+ * Dinamo
+ * 
+ * Designer:
+ * Johannes Breyer, Fabian Harb, Erkin Karamemet
+ * 
+ * Vendor URL:
+ * www.abcdinamo.com
+ * 
+ * License information:
+ * www.abcdinamo.com/information/licensing
+ */
+Cufon.registerFont({"w":0,"face":{"font-family":"","font-weight":"","font-stretch":"","font-style":"normal","units-per-em":"","panose-1":"","ascent":"","descent":"","bbox":"","underline-thickness":"","underline-position":"","unicode-range":""},"glyphs":{" ":{},"\u00a0":{}}});
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright 2019 Dinamo (Johannes Breyer, Fabian Harb) & Erkin Karamemet. All
+ * rights reserved.
+ * 
+ * Trademark:
+ * Whyte is a trademark of Dinamo (Johannes Breyer, Fabian Harb).
+ * 
+ * Full name:
+ * WhyteInktrap-Bold
+ * 
+ * Description:
+ * Please do not overuse Dinamo typefaces - keep them alive
+ * 
+ * Manufacturer:
+ * Dinamo
+ * 
+ * Designer:
+ * Johannes Breyer, Fabian Harb, Erkin Karamemet
+ * 
+ * Vendor URL:
+ * www.abcdinamo.com
+ * 
+ * License information:
+ * www.abcdinamo.com/information/licensing
+ */
+Cufon.registerFont({"w":0,"face":{"font-family":"","font-weight":"","font-stretch":"","font-style":"normal","units-per-em":"","panose-1":"","ascent":"","descent":"","bbox":"","underline-thickness":"","underline-position":"","unicode-range":""},"glyphs":{" ":{},"\u00a0":{}}});
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright 2019 Dinamo (Johannes Breyer, Fabian Harb) & Erkin Karamemet. All
+ * rights reserved.
+ * 
+ * Trademark:
+ * Whyte is a trademark of Dinamo (Johannes Breyer, Fabian Harb).
+ * 
+ * Full name:
+ * WhyteInktrap-BookItalic
+ * 
+ * Description:
+ * Please do not overuse Dinamo typefaces - keep them alive
+ * 
+ * Manufacturer:
+ * Dinamo
+ * 
+ * Designer:
+ * Johannes Breyer, Fabian Harb, Erkin Karamemet
+ * 
+ * Vendor URL:
+ * www.abcdinamo.com
+ * 
+ * License information:
+ * www.abcdinamo.com/information/licensing
+ */
+Cufon.registerFont({"w":0,"face":{"font-family":"","font-weight":"","font-stretch":"","font-style":"normal","units-per-em":"","panose-1":"","ascent":"","descent":"","bbox":"","underline-thickness":"","underline-position":"","unicode-range":""},"glyphs":{" ":{},"\u00a0":{}}});
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright 2019 Dinamo (Johannes Breyer, Fabian Harb) & Erkin Karamemet. All
+ * rights reserved.
+ * 
+ * Trademark:
+ * Whyte is a trademark of Dinamo (Johannes Breyer, Fabian Harb).
+ * 
+ * Full name:
+ * WhyteInktrap-Book
+ * 
+ * Description:
+ * Please do not overuse Dinamo typefaces - keep them alive
+ * 
+ * Manufacturer:
+ * Dinamo
+ * 
+ * Designer:
+ * Johannes Breyer, Fabian Harb, Erkin Karamemet
+ * 
+ * Vendor URL:
+ * www.abcdinamo.com
+ * 
+ * License information:
+ * www.abcdinamo.com/information/licensing
+ */
+Cufon.registerFont({"w":0,"face":{"font-family":"","font-weight":"","font-stretch":"","font-style":"normal","units-per-em":"","panose-1":"","ascent":"","descent":"","bbox":"","underline-thickness":"","underline-position":"","unicode-range":""},"glyphs":{" ":{},"\u00a0":{}}});
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright 2019 Dinamo (Johannes Breyer, Fabian Harb) & Erkin Karamemet. All
+ * rights reserved.
+ * 
+ * Trademark:
+ * Whyte is a trademark of Dinamo (Johannes Breyer, Fabian Harb).
+ * 
+ * Full name:
+ * WhyteInktrap-Light
+ * 
+ * Description:
+ * Please do not overuse Dinamo typefaces - keep them alive
+ * 
+ * Manufacturer:
+ * Dinamo
+ * 
+ * Designer:
+ * Johannes Breyer, Fabian Harb, Erkin Karamemet
+ * 
+ * Vendor URL:
+ * www.abcdinamo.com
+ * 
+ * License information:
+ * www.abcdinamo.com/information/licensing
+ */
+Cufon.registerFont({"w":0,"face":{"font-family":"","font-weight":"","font-stretch":"","font-style":"normal","units-per-em":"","panose-1":"","ascent":"","descent":"","bbox":"","underline-thickness":"","underline-position":"","unicode-range":""},"glyphs":{" ":{},"\u00a0":{}}});
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright 2019 Dinamo (Johannes Breyer, Fabian Harb) & Erkin Karamemet. All
+ * rights reserved.
+ * 
+ * Trademark:
+ * Whyte is a trademark of Dinamo (Johannes Breyer, Fabian Harb).
+ * 
+ * Full name:
+ * WhyteInktrap-Italic
+ * 
+ * Description:
+ * Please do not overuse Dinamo typefaces - keep them alive
+ * 
+ * Manufacturer:
+ * Dinamo
+ * 
+ * Designer:
+ * Johannes Breyer, Fabian Harb, Erkin Karamemet
+ * 
+ * Vendor URL:
+ * www.abcdinamo.com
+ * 
+ * License information:
+ * www.abcdinamo.com/information/licensing
+ */
+Cufon.registerFont({"w":0,"face":{"font-family":"","font-weight":"","font-stretch":"","font-style":"normal","units-per-em":"","panose-1":"","ascent":"","descent":"","bbox":"","underline-thickness":"","underline-position":"","unicode-range":""},"glyphs":{" ":{},"\u00a0":{}}});
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright 2019 Dinamo (Johannes Breyer, Fabian Harb) & Erkin Karamemet. All
+ * rights reserved.
+ * 
+ * Trademark:
+ * Whyte is a trademark of Dinamo (Johannes Breyer, Fabian Harb).
+ * 
+ * Full name:
+ * WhyteInktrap-Regular
+ * 
+ * Description:
+ * Please do not overuse Dinamo typefaces - keep them alive
+ * 
+ * Manufacturer:
+ * Dinamo
+ * 
+ * Designer:
+ * Johannes Breyer, Fabian Harb, Erkin Karamemet
+ * 
+ * Vendor URL:
+ * www.abcdinamo.com
+ * 
+ * License information:
+ * www.abcdinamo.com/information/licensing
+ */
+Cufon.registerFont({"w":0,"face":{"font-family":"","font-weight":"","font-stretch":"","font-style":"normal","units-per-em":"","panose-1":"","ascent":"","descent":"","bbox":"","underline-thickness":"","underline-position":"","unicode-range":""},"glyphs":{" ":{},"\u00a0":{}}});
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright 2019 Dinamo (Johannes Breyer, Fabian Harb) & Erkin Karamemet. All
+ * rights reserved.
+ * 
+ * Trademark:
+ * Whyte is a trademark of Dinamo (Johannes Breyer, Fabian Harb).
+ * 
+ * Full name:
+ * WhyteInktrap-LightItalic
+ * 
+ * Description:
+ * Please do not overuse Dinamo typefaces - keep them alive
+ * 
+ * Manufacturer:
+ * Dinamo
+ * 
+ * Designer:
+ * Johannes Breyer, Fabian Harb, Erkin Karamemet
+ * 
+ * Vendor URL:
+ * www.abcdinamo.com
+ * 
+ * License information:
+ * www.abcdinamo.com/information/licensing
+ */
+Cufon.registerFont({"w":0,"face":{"font-family":"","font-weight":"","font-stretch":"","font-style":"normal","units-per-em":"","panose-1":"","ascent":"","descent":"","bbox":"","underline-thickness":"","underline-position":"","unicode-range":""},"glyphs":{" ":{},"\u00a0":{}}});
